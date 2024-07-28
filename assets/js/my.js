52671eed29ece077d728c799d157234732ae8672
ThDelgado <100494591+ThDelgado@users.noreply.github.com>
2024-07-27 23:23:35 -0400

    creacion archivo

---
+++ assets/js/my.js
@@ -0,0 +1,14 @@
+function verParrafo() {
+    document.getElementById("text2").style.display = "block";
+}
+
+function ocultarParrafo() {
+    document.getElementById("text2").style.display = "none";
+}
+
+function agrandarImg() {
+    document.getElementById("img1").style.width = "100%";
+}
+function reduceImg() {
+    document.getElementById("img1").style.width = "20%";
+}
